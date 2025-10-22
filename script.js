fcfc30a59956f74fbe4075ae811fd99d2029c362
sam <sam.iannazzo@yahoo.com>
2025-10-22 10:28:28 -0700

    added const piValue / Math / console.log piValue

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-console.log ("myGreeting");
-var myGreeting = 'Hello, World!'
+console.log ("myGreeting"); 
+var myGreeting = 'Hello, World!'; // output "Hello, World" when 'myGreeting' is entered into console
+
+const piValue = Math.PI; // built in global mathematical function
+console.log(piValue); // outputs: 3.14159 when 'piValue' is entered into console using Math input
