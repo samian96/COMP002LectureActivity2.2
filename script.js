4c465e14a8c3feb3d2ca19d5e2c554950749af6c
sam <sam.iannazzo@yahoo.com>
2025-10-22 10:38:15 -0700

    added var pi / added console.log myNumber / var myNumber assigned

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log ("myGreeting"); 
 var myGreeting = 'Hello, World!'; // output "Hello, World" when 'myGreeting' is entered into console
 
-const piValue = Math.PI; // built in global mathematical function
-console.log(piValue); // outputs: 3.14159 when 'piValue' is entered into console using Math input
+const pi = Math.PI; // built in global mathematical function
+console.log(pi); // outputs: 3.14159 when 'piValue' is entered into console using Math input
+
+console.log(pi);
+var pi = 'Did you know pi starts with 3.14?'; // pi can not be redeclared, causing both the Math.PI to crash, as well as this variable for the text input to crash. This variable can only be assigned to one function.
+
+console.log(myNumber);
+var myNumber = 5;
